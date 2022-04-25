b2693a1e5d073df603c969053e65e17e5ccbc380
withdongyeong <withdongyeong@gmail.com>
2022-04-25 23:51:14 +0000

    initialize temp main, ganafont page

--- dongyeong_book/dongyeong_book/urls.py
+++ dongyeong_book/dongyeong_book/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import main
+from ganafont.views import ganafont
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', main),
+    path('ganafont', ganafont),
 ]
